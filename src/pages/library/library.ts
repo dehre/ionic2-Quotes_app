674d7d0e114c41c2d55d2d603b856cc69e6f50bd
Loris <loris4526@gmail.com>
2017-02-27 23:06:35 +0100

    Bug inside library.html --> white text inside buttons

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import data from "../../data/quotes";
+import {Quote} from "../../data/quote.interface";
+import quotes from "../../data/quotes";
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html'
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
+
+  quoteCollection:{category: string, quotes: Quote[], icon:string}[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ngOnInit(){
+    this.quoteCollection = quotes;
+  }
 
 }
