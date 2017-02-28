9bbbe0a594321349b183c4f85f15ab6243331bc8
Loris <loris4526@gmail.com>
2017-02-28 10:19:16 +0100

    Start quotes page

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {QuotesPage} from "../quotes/quotes";
 import {Quote} from "../../data/quote.interface";
 import quotes from "../../data/quotes";
 
@@ -9,6 +10,7 @@ import quotes from "../../data/quotes";
 })
 export class LibraryPage implements OnInit {
 
+  quotesPage = QuotesPage;
   quoteCollection:{category: string, quotes: Quote[], icon:string}[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
